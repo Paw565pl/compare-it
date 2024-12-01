2145204023ae3560dc5a239271d7489dad779e56
Paw565pl <paw565pl@gmail.com>
2024-12-01 16:27:00 +0100

    chore: disable csrf

--- backend/src/main/java/it/compare/backend/auth/config/SecurityConfig.java
+++ backend/src/main/java/it/compare/backend/auth/config/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +18,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement((session) -> session.sessionCreationPolicy(STATELESS));
+
         return http.build();
     }
 }
