3435a7206fd25ede837898a8564b319976e23002
Gracjan Kościński <116656782+GracjanKoscinski@users.noreply.github.com>
2024-12-12 13:17:51 +0100

    CI: create GitHub action for backend (#7)

--- backend/src/test/java/it/compare/backend/ApplicationTests.java
+++ backend/src/test/java/it/compare/backend/ApplicationTests.java
@@ -1,5 +1,8 @@
 package it.compare.backend;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,5 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTests {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertThat(2 + 2, is(4));
+    }
 }
