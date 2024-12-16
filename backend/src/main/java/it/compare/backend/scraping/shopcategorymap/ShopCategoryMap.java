e7e54a04b9c646eac148e39deb5f22c4bf6c1041
Paw565pl <paw565pl@gmail.com>
2024-12-16 23:17:02 +0100

    feat: create ShopCategoryMap class

---
+++ backend/src/main/java/it/compare/backend/scraping/shopcategorymap/ShopCategoryMap.java
@@ -0,0 +1,29 @@
+package it.compare.backend.scraping.shopcategorymap;
+
+import it.compare.backend.product.model.Category;
+import it.compare.backend.product.model.Shop;
+import java.util.EnumMap;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class ShopCategoryMap {
+
+    private final Map<Shop, Map<Category, String>> values = new EnumMap<>(Shop.class);
+
+    private ShopCategoryMap() {
+        values.put(
+                Shop.RTV_EURO_AGD,
+                Map.of(
+                        Category.GRAPHICS_CARDS,
+                        "karty-graficzne",
+                        Category.PROCESSORS,
+                        "procesory",
+                        Category.MOTHERBOARDS,
+                        "plyty-glowne",
+                        Category.RAM_MEMORY,
+                        "pamieci-ram"));
+    }
+}
