928abfc6b1c9949bbccaa00fdec77a23d26d81e8
Gracjan Kościński <116656782+GracjanKoscinski@users.noreply.github.com>
2024-12-16 10:06:06 +0100

    fix: delete unnecessary @Repository

--- backend/src/main/java/it/compare/backend/product/repository/ProductRepository.java
+++ backend/src/main/java/it/compare/backend/product/repository/ProductRepository.java
@@ -2,7 +2,5 @@ package it.compare.backend.product.repository;
 
 import it.compare.backend.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {}
