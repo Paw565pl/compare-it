b9a2143ed1d97e05d103856dea3ecd624c744d7b
Paw565pl <paw565pl@gmail.com>
2024-12-17 21:06:22 +0100

    feat: create method for getting products with ean from list

--- backend/src/main/java/it/compare/backend/product/repository/ProductRepository.java
+++ backend/src/main/java/it/compare/backend/product/repository/ProductRepository.java
@@ -1,9 +1,9 @@
 package it.compare.backend.product.repository;
 
 import it.compare.backend.product.model.Product;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
-    Optional<Product> findByEan(String ean);
+    List<Product> findAllByEanIn(List<String> eanList);
 }
