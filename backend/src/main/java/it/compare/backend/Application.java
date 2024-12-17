a0e4d272265b9f3fdabeb605eb08e0dc444b1a98
Paw565pl <paw565pl@gmail.com>
2024-12-17 12:16:27 +0100

    feat: add annotations to enable async and scheduling

--- backend/src/main/java/it/compare/backend/Application.java
+++ backend/src/main/java/it/compare/backend/Application.java
@@ -5,7 +5,11 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Application {
