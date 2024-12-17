53599985db3e9f5b9907e1feb325559dd4e03f81
Paw565pl <paw565pl@gmail.com>
2024-12-17 12:15:47 +0100

    feat: use async worker class

--- backend/src/main/java/it/compare/backend/scraping/rtvauroagd/scraper/RtvEuroAgdScraper.java
+++ backend/src/main/java/it/compare/backend/scraping/rtvauroagd/scraper/RtvEuroAgdScraper.java
@@ -1,160 +1,52 @@
 package it.compare.backend.scraping.rtvauroagd.scraper;
 
-import generator.RandomUserAgentGenerator;
-import it.compare.backend.product.model.*;
-import it.compare.backend.scraping.rtvauroagd.dto.RtvEuroAgdProduct;
-import it.compare.backend.scraping.rtvauroagd.dto.RtvEuroAgdResponse;
+import it.compare.backend.product.model.Product;
+import it.compare.backend.product.model.Shop;
 import it.compare.backend.scraping.service.ScrapingService;
 import it.compare.backend.scraping.shopcategorymap.ShopCategoryMap;
-import java.math.BigDecimal;
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 @Component
 public class RtvEuroAgdScraper {
 
-    private static final String BASE_URL = "https://www.euro.com.pl/rest/api/products/search";
-    private static final String LOGO_URL = "https://galeriachelm.com/wp-content/uploads/2020/05/logo-web-euro.png";
     private static final Shop CURRENT_SHOP = Shop.RTV_EURO_AGD;
 
-    private final SecureRandom secureRandom;
-    private final RestClient restClient;
     private final ShopCategoryMap shopCategoryMap;
     private final ScrapingService scrapingService;
+    private final RtvEuroAgdScraperWorker worker;
 
     public RtvEuroAgdScraper(
-            SecureRandom secureRandom,
-            RestClient restClient,
-            ShopCategoryMap shopCategoryMap,
-            ScrapingService scrapingService) {
-        this.secureRandom = secureRandom;
-        this.restClient = restClient;
+            ShopCategoryMap shopCategoryMap, ScrapingService scrapingService, RtvEuroAgdScraperWorker worker) {
         this.shopCategoryMap = shopCategoryMap;
         this.scrapingService = scrapingService;
+        this.worker = worker;
     }
 
+    @Async
     public void scrape() {
-        var categories = this.shopCategoryMap.getValues().get(CURRENT_SHOP);
+        var categories = shopCategoryMap.getValues().get(CURRENT_SHOP);
         var products = new ArrayList<Product>();
 
-        categories.forEach((category, categoryName) -> {
-            log.info("Starting scraping category: {}.", category);
+        var futures = categories.entrySet().stream()
+                .map(entry -> {
+                    var category = entry.getKey();
+                    var categoryValue = entry.getValue();
 
-            var categoryProducts = scrapeCategory(category, categoryName);
-            products.addAll(categoryProducts);
+                    log.info("Starting scraping category: {}.", category);
+                    return worker.scrapeCategory(category, categoryValue)
+                            .thenAccept(products::addAll)
+                            .thenRun(() -> log.info("Finished scraping category: {}.", category));
+                })
+                .toArray(CompletableFuture[]::new);
 
-            log.info("Finished scraping category: {}.", category);
+        CompletableFuture.allOf(futures).thenRun(() -> {
+            scrapingService.createProductsOrAddPriceStamp(products);
+            log.info("Saved products to database.");
         });
-
-        scrapingService.createProductsOrAddPriceStamp(products);
-        log.info("Saved products to database.");
-    }
-
-    private List<Product> scrapeCategory(Category category, String categoryName) {
-        var currentStartFrom = 0;
-        var pageSize = 25;
-
-        var products = new ArrayList<Product>();
-
-        while (true) {
-            try {
-                var uri = UriComponentsBuilder.fromUriString(BASE_URL)
-                        .queryParam("startFrom", currentStartFrom)
-                        .queryParam("numberOfItems", pageSize)
-                        .queryParam("category", categoryName)
-                        .queryParam("developSearchMode", "false")
-                        .build()
-                        .toUri();
-
-                var productsResponse = this.restClient
-                        .get()
-                        .uri(uri)
-                        .header("Accept", "application/json")
-                        .header("User-Agent", RandomUserAgentGenerator.getNext())
-                        .retrieve()
-                        .body(RtvEuroAgdResponse.class);
-                if (productsResponse == null
-                        || productsResponse.results() == null
-                        || productsResponse.results().isEmpty()) break;
-
-                productsResponse.results().forEach(productResponse -> {
-                    if (productResponse.eanCodes().isEmpty()) return;
-
-                    var outletPrices = productResponse
-                            .outletDetails()
-                            .map(outletDetails -> outletDetails.outletCategories().stream()
-                                    .map(RtvEuroAgdProduct.OutletCategory::price)
-                                    .toList());
-                    var price = getLowestPrice(productResponse.prices(), outletPrices);
-                    var promoCode = productResponse
-                            .voucherDetails()
-                            .map(RtvEuroAgdProduct.VoucherDetails::voucherCode)
-                            .orElse(null);
-                    var condition = productResponse.outletDetails().isPresent() ? Condition.OUTLET : Condition.NEW;
-
-                    var priceStamp = new PriceStamp(price, "PLN", true, condition);
-                    priceStamp.setPromoCode(promoCode);
-
-                    var offer = new Offer(CURRENT_SHOP, LOGO_URL, getProductUrl(productResponse.identifiers()));
-                    offer.getPriceHistory().add(priceStamp);
-
-                    var ean = productResponse.eanCodes().getFirst();
-                    var images = getBigImages(productResponse.images());
-
-                    var productEntity = new Product(ean, productResponse.name(), category);
-                    productEntity.setImages(images);
-                    productEntity.getOffers().add(offer);
-
-                    products.add(productEntity);
-                });
-
-                currentStartFrom += pageSize;
-                Thread.sleep(secureRandom.nextInt(500, 3000));
-            } catch (RestClientException e) {
-                log.error("Error while fetching products: {}", e.getMessage());
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                log.error(e.getMessage());
-            }
-        }
-
-        return products;
-    }
-
-    private BigDecimal getLowestPrice(RtvEuroAgdProduct.Prices prices, Optional<List<Long>> outletPrices) {
-        var priceList = new ArrayList<Long>();
-
-        if (prices.mainPrice() != null) priceList.add(prices.mainPrice());
-        if (prices.promotionalPrice() != null) priceList.add(prices.promotionalPrice());
-        if (prices.voucherDiscountedPrice() != null) priceList.add(prices.voucherDiscountedPrice());
-
-        outletPrices.ifPresent(priceList::addAll);
-
-        return priceList.stream()
-                .filter(Objects::nonNull)
-                .map(BigDecimal::new)
-                .min(BigDecimal::compareTo)
-                .orElse(null);
-    }
-
-    private String getProductUrl(RtvEuroAgdProduct.Identifiers identifiers) {
-        var baseUrl = "https://www.euro.com.pl";
-        return baseUrl + "/" + identifiers.productGroupLinkName() + "/" + identifiers.productLinkName() + ".bhtml";
-    }
-
-    private List<String> getBigImages(List<RtvEuroAgdProduct.Image> images) {
-        return images.stream()
-                .filter((image -> image.type().equalsIgnoreCase("BIG_PHOTO")))
-                .map(RtvEuroAgdProduct.Image::url)
-                .toList();
     }
 }
