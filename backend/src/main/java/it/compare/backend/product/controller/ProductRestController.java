098d39969493d0bb6f8d9996e889490a302c7726
Gracjan Koscinski <gracek247927@gmail.com>
2024-12-18 22:06:03 +0100

    fix: add version to api request mapping

--- backend/src/main/java/it/compare/backend/product/controller/ProductRestController.java
+++ backend/src/main/java/it/compare/backend/product/controller/ProductRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductRestController {
 
