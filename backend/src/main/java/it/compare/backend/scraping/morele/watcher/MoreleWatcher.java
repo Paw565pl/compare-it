1cf758b6f7e66ec5bead355516737f187fc50933
Damian <d.rogalski.417@studms.ug.edu.pl>
2024-12-19 00:43:07 +0100

    feat: add scheduler

--- backend/src/main/java/it/compare/backend/scraping/morele/watcher/MoreleWatcher.java
+++ backend/src/main/java/it/compare/backend/scraping/morele/watcher/MoreleWatcher.java
@@ -1,3 +1,22 @@
 package it.compare.backend.scraping.morele.watcher;
 
-public class MoreleWatcher {}
+import it.compare.backend.scraping.morele.scraper.MoreleScraper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MoreleWatcher {
+
+    private final MoreleScraper moreleScraper;
+
+    public MoreleWatcher(MoreleScraper moreleScraper) {
+        this.moreleScraper = moreleScraper;
+    }
+
+    @Scheduled(cron = "0 0 */2 * * *")
+    public void startTask() {
+        moreleScraper.scrape();
+    }
+}
