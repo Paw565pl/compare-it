92f43ab4cd43d1099ab2b453854ae8f74d6c0b88
Paw565pl <paw565pl@gmail.com>
2024-12-19 19:23:49 +0100

    chore: fix eslint error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import "./globals.css";
+import "@/app/globals.css";
 
 export const metadata: Metadata = {
   title: "compare.it",
