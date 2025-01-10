a47eee047f35857508a1ef8c44492ee9001ca1d5
Paw565pl <paw565pl@gmail.com>
2025-01-10 20:46:16 +0100

    feat: setup auth0 auth provider

--- frontend/src/auth/config/auth-config.ts
+++ frontend/src/auth/config/auth-config.ts
@@ -1,5 +1,46 @@
+import { Tokens } from "@/auth/types/tokens";
+import { User } from "@/auth/types/user";
 import NextAuth from "next-auth";
+import Auth0, { Auth0Profile } from "next-auth/providers/auth0";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Auth0({
+      authorization: {
+        params: { scope: "openid profile email offline_access" },
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: ({ token, account, profile }) => {
+      const auth0Profile = profile as Auth0Profile | undefined;
+
+      if (account && auth0Profile) {
+        console.log("account");
+        console.log(account);
+
+        const user: User = {
+          id: auth0Profile?.sub || "",
+          username: auth0Profile?.preferred_username,
+          email: auth0Profile?.email,
+          picture: auth0Profile?.picture,
+          roles: profile?.realm_access?.roles,
+        };
+
+        const tokens: Tokens = {
+          accessToken: account.access_token,
+          accessTokenExpiresIn: account.expires_in,
+          accessTokenExpiresAt: account.expires_at,
+          refreshToken: account.refresh_token,
+        };
+
+        return { user, tokens };
+      } else {
+        return token;
+      }
+    },
+    session: ({ session, token }) => {
+      return { ...session, ...token };
+    },
+  },
 });
