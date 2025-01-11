bce4553c2454e828505a303818c8e30818e9fa40
Paw565pl <paw565pl@gmail.com>
2025-01-11 11:01:30 +0100

    feat: adjust user type

--- frontend/src/auth/types/user.ts
+++ frontend/src/auth/types/user.ts
@@ -2,8 +2,8 @@ import { Role } from "@/auth/types/role";
 
 export interface User {
   readonly id: string;
-  readonly username?: string | null;
-  readonly email?: string | null;
-  readonly picture?: string | null;
+  readonly username: string;
+  readonly email: string;
+  readonly picture: string;
   readonly roles?: Role[];
 }
