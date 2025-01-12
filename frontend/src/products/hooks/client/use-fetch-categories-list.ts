d35f9da2e87a3d8741493840f79b13c784525764
Paw565pl <paw565pl@gmail.com>
2025-01-12 11:21:57 +0100

    refactor: move file to query options dir

--- frontend/src/products/hooks/client/use-fetch-categories-list.ts
+++ frontend/src/products/hooks/client/use-fetch-categories-list.ts
@@ -1,4 +1,4 @@
-import { categoriesListQueryOptions } from "@/products/hooks/categories-list-query-options";
+import { categoriesListQueryOptions } from "@/products/hooks/query-options/categories-list-query-options";
 import { useQuery } from "@tanstack/react-query";
 
 export const useFetchCategoriesList = () =>
--- frontend/src/products/hooks/server/prefetch-categories-list.ts
+++ frontend/src/products/hooks/server/prefetch-categories-list.ts
@@ -1,4 +1,4 @@
-import { categoriesListQueryOptions } from "@/products/hooks/categories-list-query-options";
+import { categoriesListQueryOptions } from "@/products/hooks/query-options/categories-list-query-options";
 import { QueryClient } from "@tanstack/react-query";
 
 export const prefetchCategoriesList = (queryClient: QueryClient) =>
