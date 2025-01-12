fe5ac13262b1067329537c35b2862d86b33117d6
Gracjan Kościński <116656782+GracjanKoscinski@users.noreply.github.com>
2025-01-12 22:20:06 +0100

    FEAT(backend): implement sorting and filter on product list (#26)

--- backend/src/main/java/it/compare/backend/product/controller/ProductRestController.java
+++ backend/src/main/java/it/compare/backend/product/controller/ProductRestController.java
@@ -1,15 +1,13 @@
 package it.compare.backend.product.controller;
 
+import it.compare.backend.product.dto.ProductFiltersDto;
 import it.compare.backend.product.response.ProductDetailResponse;
 import it.compare.backend.product.response.ProductListResponse;
 import it.compare.backend.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -19,8 +17,8 @@ public class ProductRestController {
     private final ProductService productService;
 
     @GetMapping
-    public Page<ProductListResponse> findAll(Pageable pageable) {
-        return productService.findAll(pageable);
+    public Page<ProductListResponse> findAll(ProductFiltersDto filters, Pageable pageable) {
+        return productService.findAll(filters, pageable);
     }
 
     @GetMapping("/{id}")
