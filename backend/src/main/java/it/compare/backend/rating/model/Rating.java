7936b1105934f8d0abb52189785728bf30728802
Paw565pl <paw565pl@gmail.com>
2025-01-15 18:56:57 +0100

    feat: create rating model

---
+++ backend/src/main/java/it/compare/backend/rating/model/Rating.java
@@ -0,0 +1,29 @@
+package it.compare.backend.rating.model;
+
+import it.compare.backend.auth.model.User;
+import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Rating {
+
+    @DocumentReference
+    @Field("author")
+    private User author;
+
+    @Field("isUpVote")
+    @NonNull private Boolean isUpVote;
+
+    @Indexed
+    @CreatedDate
+    @Field("createdAt")
+    private LocalDateTime createdAt;
+}
