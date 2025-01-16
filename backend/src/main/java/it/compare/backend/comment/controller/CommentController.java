21a7d27ea6b292720a8e1bde235070e70f0a9f49
Paw565pl <paw565pl@gmail.com>
2025-01-16 13:16:25 +0100

    feat: create comment controller

---
+++ backend/src/main/java/it/compare/backend/comment/controller/CommentController.java
@@ -0,0 +1,41 @@
+package it.compare.backend.comment.controller;
+
+import it.compare.backend.auth.annotation.IsAuthenticated;
+import it.compare.backend.auth.details.OAuthUserDetails;
+import it.compare.backend.comment.dto.CommentDto;
+import it.compare.backend.comment.response.CommentResponse;
+import it.compare.backend.comment.service.CommentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products/{productId}/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping
+    public Page<CommentResponse> findAll(@PathVariable String productId, Pageable pageable) {
+        return commentService.findAll(productId, pageable);
+    }
+
+    @GetMapping("/{commentId}")
+    public CommentResponse findById(@PathVariable String productId, @PathVariable String commentId) {
+        return commentService.findById(productId, commentId);
+    }
+
+    @IsAuthenticated
+    @PostMapping
+    public CommentResponse create(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable String productId,
+            @Valid @RequestBody CommentDto commentDto) {
+        return commentService.create(OAuthUserDetails.fromJwt(jwt), productId, commentDto);
+    }
+}
