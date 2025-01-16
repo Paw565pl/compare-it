fecdf957b0b22e85995b24d241ba915357c02494
Paw565pl <paw565pl@gmail.com>
2025-01-16 13:15:58 +0100

    feat: create comment repository

--- backend/src/main/java/it/compare/backend/comment/repository/CommentRepository.java
+++ backend/src/main/java/it/compare/backend/comment/repository/CommentRepository.java
@@ -1,7 +1,10 @@
 package it.compare.backend.comment.repository;
 
 import it.compare.backend.comment.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
+    Page<Comment> findAllByProductId(String productId, Pageable pageable);
 }
