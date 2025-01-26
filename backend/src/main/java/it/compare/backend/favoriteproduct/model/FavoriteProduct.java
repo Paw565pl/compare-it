73fdd1a83674b01fc5e67f62ed90a6453d986540
Paw565pl <paw565pl@gmail.com>
2025-01-26 19:49:23 +0100

    feat: create FavoriteProduct document class

---
+++ backend/src/main/java/it/compare/backend/favoriteproduct/model/FavoriteProduct.java
@@ -0,0 +1,43 @@
+package it.compare.backend.favoriteproduct.model;
+
+import it.compare.backend.auth.model.User;
+import it.compare.backend.product.model.Product;
+import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Document(collection = "favorite_products")
+@CompoundIndex(
+        name = "unique_favorite_product_per_user_index",
+        def = "{'user.$id': 1, 'product.$id': 1}",
+        unique = true)
+public class FavoriteProduct {
+
+    @MongoId
+    @Field("_id")
+    private String id;
+
+    @DBRef(lazy = true)
+    @Field("user")
+    @NonNull private User user;
+
+    @DBRef(lazy = true)
+    @Field("product")
+    @NonNull private Product product;
+
+    @Indexed
+    @CreatedDate
+    @Field("createdAt")
+    private LocalDateTime createdAt;
+}
