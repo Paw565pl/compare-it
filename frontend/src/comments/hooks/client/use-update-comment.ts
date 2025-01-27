1c61a162fb4e04d7057f9c5429c3bc909cfb968e
Paw565pl <paw565pl@gmail.com>
2025-01-27 17:39:19 +0100

    feat: create hook for updating comment

---
+++ frontend/src/comments/hooks/client/use-update-comment.ts
@@ -0,0 +1,55 @@
+import { CommentDto } from "@/comments/dtos/comment-dto";
+import { CommentEntity } from "@/comments/entities/comment-entity";
+import { commentsQueryKey } from "@/comments/hooks/query-options/comments-query-key";
+import { getQueryClient } from "@/core/libs/tanstack-query";
+import { apiService } from "@/core/services/api";
+import { ErrorResponse } from "@/core/services/api/types/error-response";
+import { productsQueryKey } from "@/products/hooks/query-options/products-query-key";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+const updateComment = async (
+  accessToken: string,
+  productId: string,
+  commentId: string,
+  commentDto: CommentDto,
+) => {
+  const { data } = await apiService.put<CommentEntity>(
+    `/v1/products/${productId}/comments/${commentId}`,
+    commentDto,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  return data;
+};
+
+export const useUpdateComment = (
+  accessToken: string,
+  productId: string,
+  commentId: string,
+) =>
+  useMutation<CommentEntity, AxiosError<ErrorResponse>, CommentDto>({
+    mutationKey: [
+      ...productsQueryKey,
+      productId,
+      ...commentsQueryKey,
+      commentId,
+      "update",
+    ] as const,
+    mutationFn: (commentDto) =>
+      updateComment(accessToken, productId, commentId, commentDto),
+    onSuccess: () => {
+      const queryClient = getQueryClient();
+      const queryKey = [
+        ...productsQueryKey,
+        productId,
+        ...commentsQueryKey,
+        commentId,
+      ] as const;
+
+      queryClient.invalidateQueries({ queryKey });
+    },
+  });
