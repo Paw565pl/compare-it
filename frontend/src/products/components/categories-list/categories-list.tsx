d0167260d63760ddf53b474fa4d68e2beae7f7e3
Damian <d.rogalski.417@studms.ug.edu.pl>
2025-01-28 18:37:44 +0100

    feat: add highlighting to current category

--- frontend/src/products/components/categories-list/categories-list.tsx
+++ frontend/src/products/components/categories-list/categories-list.tsx
@@ -5,32 +5,40 @@ import { parseAsFloat, parseAsString, useQueryStates } from "nuqs";
 const CategoriesList = () => {
   const { data: categoriesList } = useFetchCategoriesList();
 
-  const [, setFilters] = useQueryStates({
+  const [filters, setFilters] = useQueryStates({
     category: parseAsString,
     minPrice: parseAsFloat,
     maxPrice: parseAsFloat,
     shop: parseAsString,
     name: parseAsString,
   });
+
   return (
     <div className="flex flex-col items-start">
       <h2 className="mb-1 ml-4 text-2xl font-bold text-secondary sm:ml-0">
         Kategorie
       </h2>
       <ul className="mt-2 w-full bg-white">
-        {categoriesList?.map((category, index) => (
-          <li
-            key={index}
-            className="cursor-pointer transition-colors duration-200 hover:bg-hover hover:text-white"
-          >
-            <button
-              onClick={() => setFilters({ category: category })}
-              className="flex w-full justify-start px-4 py-2"
+        {categoriesList?.map((category, index) => {
+          const isActive = filters.category === category;
+          return (
+            <li
+              key={index}
+              className={`cursor-pointer transition-colors duration-200 ${
+                isActive
+                  ? "bg-hover text-white"
+                  : "hover:bg-hover hover:text-white"
+              }`}
             >
-              <div>{category}</div>
-            </button>
-          </li>
-        ))}
+              <button
+                onClick={() => setFilters({ category: category })}
+                className="flex w-full justify-start px-4 py-2"
+              >
+                <div>{category}</div>
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
