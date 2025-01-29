cf0179dbbf367a88ecfb38cd7e9281365ccddcf5
Paw565pl <paw565pl@gmail.com>
2025-01-29 15:13:37 +0100

    feat: add prefetch of favorite products

--- frontend/src/profile/pages/profile-page.tsx
+++ frontend/src/profile/pages/profile-page.tsx
@@ -1,5 +1,20 @@
+import { auth } from "@/auth/config/auth-config";
+import { getQueryClient } from "@/core/libs/tanstack-query";
+import { prefetchFavoriteProductsPage } from "@/favorite-products/hooks/server/prefetch-favorite-products-page";
 import { Profile } from "@/profile/components";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export const ProfilePage = () => {
-  return <Profile />;
+export const ProfilePage = async () => {
+  const session = await auth();
+  const accessToken = session?.tokens?.accessToken as string;
+  const userId = session?.user?.id as string;
+
+  const queryClient = getQueryClient();
+  await prefetchFavoriteProductsPage(queryClient, accessToken, userId);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Profile />
+    </HydrationBoundary>
+  );
 };
