66236b56a4e9cec67824569b391c2cda4dd7d366
Paw565pl <paw565pl@gmail.com>
2025-01-30 16:24:50 +0100

    feat: implement updating price alert

--- frontend/src/price-alerts/components/price-alert-card/price-alert-card.tsx
+++ frontend/src/price-alerts/components/price-alert-card/price-alert-card.tsx
@@ -9,19 +9,49 @@ import {
   CardTitle,
 } from "@/core/components/ui/card";
 import { PriceAlertFormDialog } from "@/price-alerts/components";
+import { PriceAlertDto } from "@/price-alerts/dtos/price-alert-dto";
 import { PriceAlertEntity } from "@/price-alerts/entities/price-alert-entity";
+import { useDeletePriceAlert } from "@/price-alerts/hooks/client/use-delete-price-alert";
+import { useUpdatePriceAlert } from "@/price-alerts/hooks/client/use-update-price-alert";
+import { PriceAlertFormValues } from "@/price-alerts/schemas/price-alert-schema";
 import { ProductImage } from "@/products/components";
 import { useFetchProduct } from "@/products/hooks/client/use-fetch-product";
 import { formatCurrency } from "@/products/utils/format-currency";
 import { Pen } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { toast } from "sonner";
 
 interface PriceAlertCardProps {
   priceAlert: PriceAlertEntity;
 }
 
 export const PriceAlertCard = ({ priceAlert }: PriceAlertCardProps) => {
+  const { data: session } = useSession();
+  const accessToken = session?.tokens?.accessToken as string;
+
   const { data: product } = useFetchProduct(priceAlert.productId);
+  const { mutate: updatePriceAlert } = useUpdatePriceAlert(
+    accessToken,
+    priceAlert.id,
+  );
+  const { mutate: deletePriceAlert } = useDeletePriceAlert(
+    accessToken,
+    priceAlert.id,
+  );
+
+  const handleUpdatePriceAlert = (formValues: PriceAlertFormValues) => {
+    const priceAlertDto: PriceAlertDto = {
+      productId: priceAlert.productId,
+      targetPrice: Number(formValues.targetPrice),
+      isOutletAllowed: formValues.isOutletAllowed,
+    };
+
+    updatePriceAlert(priceAlertDto, {
+      onSuccess: () => toast.success("Alert cenowy został zaktualizowany."),
+      onError: () => toast.error("Coś poszło nie tak!"),
+    });
+  };
 
   const formattedDesiredPrice = formatCurrency(priceAlert.targetPrice, "PLN");
 
@@ -31,7 +61,7 @@ export const PriceAlertCard = ({ priceAlert }: PriceAlertCardProps) => {
         <div className="flex justify-center">
           <ProductImage
             name={priceAlert.productName}
-            imageUrl={product?.images.at(0) || ""}
+            imageUrl={product?.images.at(0) || null}
           />
         </div>
 
@@ -71,7 +101,11 @@ export const PriceAlertCard = ({ priceAlert }: PriceAlertCardProps) => {
             </Button>
           }
           dialogHeader="Edytuj alert cenowy"
-          handleSubmit={() => console.log("submit")}
+          handleSubmit={handleUpdatePriceAlert}
+          defaultValues={{
+            targetPrice: priceAlert.targetPrice,
+            isOutletAllowed: priceAlert.outletAllowed,
+          }}
         />
       </CardFooter>
     </Card>
