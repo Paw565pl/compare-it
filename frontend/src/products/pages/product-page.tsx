f76171c2fbda841cda6edd74ddedd8a3237c5cb7
Paw565pl <paw565pl@gmail.com>
2025-02-02 15:01:40 +0100

    feat: add prefetch for product details page

--- frontend/src/products/pages/product-page.tsx
+++ frontend/src/products/pages/product-page.tsx
@@ -1,4 +1,8 @@
+import { prefetchCommentPage } from "@/comments/hooks/server/prefetch-comment-page";
+import { getQueryClient } from "@/core/libs/tanstack-query";
 import { ProductPageTop } from "@/products/components/index";
+import { prefetchProduct } from "@/products/hooks/server/prefetch-product";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 export interface ProductPageProps {
   readonly params: Promise<{ id: string }>;
@@ -7,9 +11,18 @@ export interface ProductPageProps {
 export const ProductPage = async ({ params }: ProductPageProps) => {
   const productId = (await params).id;
 
+  const queryClient = getQueryClient();
+
+  await Promise.all([
+    await prefetchProduct(queryClient, productId),
+    await prefetchCommentPage(queryClient, productId),
+  ]);
+
   return (
-    <div className="flex flex-col">
-      <ProductPageTop productId={productId} />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-col">
+        <ProductPageTop productId={productId} />
+      </div>
+    </HydrationBoundary>
   );
 };
