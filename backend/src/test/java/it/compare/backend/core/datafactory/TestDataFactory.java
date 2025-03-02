d18b82fb2e29111c4a82efb513599560f781b9b8
Paw565pl <paw565pl@gmail.com>
2025-03-02 14:39:03 +0100

    chore: change return type

--- backend/src/test/java/it/compare/backend/core/datafactory/TestDataFactory.java
+++ backend/src/test/java/it/compare/backend/core/datafactory/TestDataFactory.java
@@ -1,8 +1,12 @@
 package it.compare.backend.core.datafactory;
 
+import java.util.Collection;
+
 public interface TestDataFactory<T> {
 
     T createOne();
 
     Iterable<T> createMany(int count);
+    Collection<T> createMany(int count);
+
 }
