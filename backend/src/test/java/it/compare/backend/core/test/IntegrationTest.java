b664c7a4aa6f1b7d7fa0327511be5bca255bb6ee
Paw565pl <paw565pl@gmail.com>
2025-03-02 14:39:56 +0100

    chore: remove context annotation

--- backend/src/test/java/it/compare/backend/core/test/IntegrationTest.java
+++ backend/src/test/java/it/compare/backend/core/test/IntegrationTest.java
@@ -1,13 +1,11 @@
 package it.compare.backend.core.test;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 import io.restassured.RestAssured;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -17,7 +15,6 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 public abstract class IntegrationTest {
 
     @LocalServerPort
