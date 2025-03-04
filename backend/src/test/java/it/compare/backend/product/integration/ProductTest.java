388a22d1bfb858f5f6501f54cfe1ba9289e6d2b8
Paw565pl <paw565pl@gmail.com>
2025-03-04 12:01:30 +0100

    chore: remove ProductRepository from ProductTest base class

--- backend/src/test/java/it/compare/backend/product/integration/ProductTest.java
+++ backend/src/test/java/it/compare/backend/product/integration/ProductTest.java
@@ -2,7 +2,6 @@ package it.compare.backend.product.integration;
 
 import it.compare.backend.core.test.IntegrationTest;
 import it.compare.backend.product.datafactory.ProductTestDataFactory;
-import it.compare.backend.product.repository.ProductRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +12,6 @@ abstract class ProductTest extends IntegrationTest {
 
     @Autowired
     ProductTestDataFactory productTestDataFactory;
-
-    @Autowired
-    ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
