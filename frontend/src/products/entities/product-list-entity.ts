fee9b8bce81b5519474042950f2754f1c728374c
Paw565pl <paw565pl@gmail.com>
2025-03-24 13:23:10 +0100

    fix: update product entities

--- frontend/src/products/entities/product-list-entity.ts
+++ frontend/src/products/entities/product-list-entity.ts
@@ -1,12 +1,15 @@
+import { CategoryEntity } from "@/products/entities/category-entity";
+import { ShopEntity } from "@/products/entities/shop-entity";
+
 export interface ProductListEntity {
   readonly id: string;
   readonly name: string;
   readonly ean: string;
-  readonly category: string;
+  readonly category: CategoryEntity;
   readonly mainImageUrl: string | null;
-  readonly lowestCurrentPrice: number;
-  readonly lowestPriceCurrency: string;
-  readonly lowestPriceShop: string;
-  readonly offerCount: number;
+  readonly lowestCurrentPrice: number | null;
+  readonly lowestPriceCurrency: string | null;
+  readonly lowestPriceShop: ShopEntity | null;
+  readonly offersCount: number;
   readonly isAvailable: boolean;
 }
