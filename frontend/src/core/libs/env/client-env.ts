40d5b5b38160c51ad8868673fe90116a0db1bfa5
Paw565pl <paw565pl@gmail.com>
2025-03-26 17:30:32 +0100

    feat: set session refetch interval

--- frontend/src/core/libs/env/client-env.ts
+++ frontend/src/core/libs/env/client-env.ts
@@ -6,11 +6,14 @@ const clientEnv = createEnv({
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
     NEXT_PUBLIC_AUTH_AUTH0_ISSUER: z.string().url(),
     NEXT_PUBLIC_AUTH_AUTH0_ID: z.string(),
+    NEXT_PUBLIC_ACCESS_TOKEN_LIFETIME: z.number().positive(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_AUTH_AUTH0_ISSUER: process.env.NEXT_PUBLIC_AUTH_AUTH0_ISSUER,
     NEXT_PUBLIC_AUTH_AUTH0_ID: process.env.NEXT_PUBLIC_AUTH_AUTH0_ID,
+    NEXT_PUBLIC_ACCESS_TOKEN_LIFETIME:
+      process.env.NEXT_PUBLIC_ACCESS_TOKEN_LIFETIME,
   },
   skipValidation: process.env.CI ? true : false,
 });
