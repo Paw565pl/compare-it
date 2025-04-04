f0bb115bbd9f8cd6d8924ff029712b0a8ef5b6f8
Paw565pl <paw565pl@gmail.com>
2025-04-04 20:27:05 +0200

    feat: create MediaExpertScraper

---
+++ backend/src/main/java/it/compare/backend/scraping/mediaexpert/scraper/MediaExpertScraper.java
@@ -0,0 +1,37 @@
+package it.compare.backend.scraping.mediaexpert.scraper;
+
+import it.compare.backend.product.model.Shop;
+import it.compare.backend.scraping.service.ScrapingService;
+import it.compare.backend.scraping.shopcategorymap.ShopCategoryMap;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MediaExpertScraper {
+
+    private static final Shop CURRENT_SHOP = Shop.MEDIA_EXPERT;
+
+    private final ShopCategoryMap shopCategoryMap;
+    private final ScrapingService scrapingService;
+    private final MediaExpertScraperWorker worker;
+
+    @Async
+    public void scrape() {
+        var categories = shopCategoryMap.getValues().get(CURRENT_SHOP);
+        categories.forEach((category, categoryName) -> {
+            log.info("started scraping category: {}", category);
+
+            worker.scrapeCategory(category, categoryName)
+                    .thenAccept(scrapingService::createProductsOrAddPriceStamp)
+                    .thenRun(() -> log.info("finished scraping category: {}", category))
+                    .exceptionally(e -> {
+                        log.error("error while scraping category {}: {}", category, e.getMessage());
+                        return null;
+                    });
+        });
+    }
+}
