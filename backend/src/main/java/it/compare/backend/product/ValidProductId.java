4532534c2c596e2e78f9a3cf8f71db106ce84677
Paw565pl <paw565pl@gmail.com>
2025-04-19 19:03:32 +0200

    feat: create composite validation annotation for product id

---
+++ backend/src/main/java/it/compare/backend/product/ValidProductId.java
@@ -0,0 +1,24 @@
+package it.compare.backend.product;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import it.compare.backend.core.validator.ValidObjectId;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({FIELD, PARAMETER})
+@Constraint(validatedBy = {})
+@NotBlank(message = "productId cannot be empty.") @ValidObjectId(message = "productId must be a valid ObjectId.")
+public @interface ValidProductId {
+    String message() default "Invalid productId format.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
