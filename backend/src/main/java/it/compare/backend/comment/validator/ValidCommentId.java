744789b288e9a956132f1eb191a7c6d9e8aacd44
Paw565pl <paw565pl@gmail.com>
2025-04-19 19:51:30 +0200

    chore: reformat annotations

--- backend/src/main/java/it/compare/backend/comment/validator/ValidCommentId.java
+++ backend/src/main/java/it/compare/backend/comment/validator/ValidCommentId.java
@@ -1,22 +1,20 @@
 package it.compare.backend.comment.validator;
 
-import it.compare.backend.core.validator.ValidObjectId;
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-import jakarta.validation.constraints.NotBlank;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import it.compare.backend.core.validator.ValidObjectId;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 @Retention(RUNTIME)
 @Target({FIELD, PARAMETER})
 @Constraint(validatedBy = {})
-@NotBlank(message = "commentId cannot be empty.")
-@ValidObjectId(message = "commentId must be a valid ObjectId.")
+@NotBlank(message = "commentId cannot be empty.") @ValidObjectId(message = "commentId must be a valid ObjectId.")
 public @interface ValidCommentId {
     String message() default "Invalid commentId format.";
 
--- backend/src/main/java/it/compare/backend/pricealert/validator/ValidPriceAlertId.java
+++ backend/src/main/java/it/compare/backend/pricealert/validator/ValidPriceAlertId.java
@@ -1,22 +1,20 @@
 package it.compare.backend.pricealert.validator;
 
-import it.compare.backend.core.validator.ValidObjectId;
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-import jakarta.validation.constraints.NotBlank;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import it.compare.backend.core.validator.ValidObjectId;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 @Retention(RUNTIME)
 @Target({FIELD, PARAMETER})
 @Constraint(validatedBy = {})
-@NotBlank(message = "priceAlertId cannot be empty.")
-@ValidObjectId(message = "priceAlertId must be a valid ObjectId.")
+@NotBlank(message = "priceAlertId cannot be empty.") @ValidObjectId(message = "priceAlertId must be a valid ObjectId.")
 public @interface ValidPriceAlertId {
     String message() default "Invalid priceAlertId format.";
 
