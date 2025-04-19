e065d0be469def0d7e65f754c767f24c73421db8
Paw565pl <paw565pl@gmail.com>
2025-04-19 19:50:04 +0200

    feat: create composite validation annotation for comment id

---
+++ backend/src/main/java/it/compare/backend/comment/validator/ValidCommentId.java
@@ -0,0 +1,26 @@
+package it.compare.backend.comment.validator;
+
+import it.compare.backend.core.validator.ValidObjectId;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({FIELD, PARAMETER})
+@Constraint(validatedBy = {})
+@NotBlank(message = "commentId cannot be empty.")
+@ValidObjectId(message = "commentId must be a valid ObjectId.")
+public @interface ValidCommentId {
+    String message() default "Invalid commentId format.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
