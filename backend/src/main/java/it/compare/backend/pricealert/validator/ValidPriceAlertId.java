47f05b499f05249faf864afff07dbc0547ab6b02
Paw565pl <paw565pl@gmail.com>
2025-04-19 19:50:18 +0200

    feat: create composite validation annotation for price alert id

---
+++ backend/src/main/java/it/compare/backend/pricealert/validator/ValidPriceAlertId.java
@@ -0,0 +1,26 @@
+package it.compare.backend.pricealert.validator;
+
+import it.compare.backend.core.validator.ValidObjectId;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({FIELD, PARAMETER})
+@Constraint(validatedBy = {})
+@NotBlank(message = "priceAlertId cannot be empty.")
+@ValidObjectId(message = "priceAlertId must be a valid ObjectId.")
+public @interface ValidPriceAlertId {
+    String message() default "Invalid priceAlertId format.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
