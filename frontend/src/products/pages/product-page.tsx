e6bd7bb0a40db60f59ca4a15e7aba7989149f81f
Paw565pl <paw565pl@gmail.com>
2025-04-24 18:29:23 +0200

    feat: add conditional prefetch

--- frontend/src/products/pages/product-page.tsx
+++ frontend/src/products/pages/product-page.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@/auth/config/auth-config";
 import { prefetchCommentPage } from "@/comments/hooks/server/prefetch-comment-page";
 import { getQueryClient } from "@/core/libs/tanstack-query";
+import { prefetchFavoriteProductStatus } from "@/favorite-products/hooks/server/prefetch-favorite-product-status";
+import { prefetchActivePriceAlertForProduct } from "@/price-alerts/hooks/server/prefetch-active-price-alert-for-product";
 import { ProductPageTop } from "@/products/components/index";
 import { prefetchProduct } from "@/products/hooks/server/prefetch-product";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
@@ -11,12 +13,32 @@ export interface ProductPageProps {
 
 export const ProductPage = async ({ params }: ProductPageProps) => {
   const productId = (await params).id;
+
   const session = await auth();
+  const accessToken = session?.tokens?.accessToken as string;
+  const userId = session?.user?.id as string;
 
   const queryClient = getQueryClient();
 
-  prefetchCommentPage(queryClient, productId, session?.tokens?.accessToken);
-  await prefetchProduct(queryClient, productId);
+  const queries = [prefetchProduct(queryClient, productId)];
+  if (session)
+    queries.push(
+      prefetchFavoriteProductStatus(
+        queryClient,
+        accessToken,
+        userId,
+        productId,
+      ),
+      prefetchActivePriceAlertForProduct(
+        queryClient,
+        accessToken,
+        userId,
+        productId,
+      ),
+    );
+
+  await Promise.all(queries);
+  prefetchCommentPage(queryClient, productId, accessToken);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
