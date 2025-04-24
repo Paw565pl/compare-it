69b6ca1729749f91f6d2c80d028ded139f0cfbe5
Paw565pl <paw565pl@gmail.com>
2025-04-24 17:50:28 +0200

    feat: add notblank annotations directly to dtos

--- backend/src/main/java/it/compare/backend/pricealert/dto/PriceAlertDto.java
+++ backend/src/main/java/it/compare/backend/pricealert/dto/PriceAlertDto.java
@@ -1,11 +1,12 @@
 package it.compare.backend.pricealert.dto;
 
 import it.compare.backend.product.validator.ValidProductId;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 public record PriceAlertDto(
-        @ValidProductId String productId,
+        @NotBlank(message = "productId cannot be empty.") @ValidProductId String productId,
         @NotNull(message = "targetPrice cannot be null.") @Positive(message = "targetPrice must be greater than zero.") BigDecimal targetPrice,
         @NotNull(message = "isOutletAllowed cannot be null.") Boolean isOutletAllowed) {}
