8af1cfc2d9a5066244a431108dfcd70eb69e2dcb
Paw565pl <paw565pl@gmail.com>
2025-04-24 09:32:01 +0200

    feat: launch scraping on startup if a database is empty

--- backend/src/main/java/it/compare/backend/Application.java
+++ backend/src/main/java/it/compare/backend/Application.java
@@ -2,9 +2,13 @@ package it.compare.backend;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+import it.compare.backend.product.repository.ProductRepository;
+import it.compare.backend.scraping.scraper.ScrapingManager;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -20,5 +24,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ProductRepository productRepository, ScrapingManager scrapingManager) {
+        return args -> {
+            if (productRepository.count() == 0) scrapingManager.scrapeAll();
+        };
     }
 }
