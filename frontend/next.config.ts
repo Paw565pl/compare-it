7deb85bce52f2a3f6a261a1ef255bb2aa92bd544
Paw565pl <paw565pl@gmail.com>
2025-04-26 17:52:36 +0200

    feat: update dependencies and enable react compiler

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  experimental: {
+    reactCompiler: true,
+  },
   images: {
     remotePatterns: [
       {
