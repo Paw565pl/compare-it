17fcc40dc15e1be36616c9633482e3eb0a9fe302
Paw565pl <paw565pl@gmail.com>
2025-04-26 14:51:50 +0200

    refactor: improve flexibility of format date util function

--- frontend/src/core/utils/format-date.ts
+++ frontend/src/core/utils/format-date.ts
@@ -1,5 +1,12 @@
-export const formatDate = (date: string) =>
-  new Intl.DateTimeFormat("pl-PL", {
-    dateStyle: "medium",
-    timeStyle: "short",
-  }).format(new Date(date));
+const defaultOptions: Intl.DateTimeFormatOptions = {
+  dateStyle: "medium",
+  timeStyle: "short",
+} as const;
+
+export const formatDate = (
+  date: string,
+  options?: Intl.DateTimeFormatOptions,
+) =>
+  new Intl.DateTimeFormat("pl-PL", options ?? defaultOptions).format(
+    new Date(date),
+  );
