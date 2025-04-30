7d8e0f04eeb7117b91ba2e34763a55f924b5dd12
Paw565pl <paw565pl@gmail.com>
2025-04-30 14:56:41 +0200

    feat: use semaphore and use helper methods to improve readability

--- backend/src/main/java/it/compare/backend/scraping/rtveuroagd/scraper/RtvEuroAgdScraperWorker.java
+++ backend/src/main/java/it/compare/backend/scraping/rtveuroagd/scraper/RtvEuroAgdScraperWorker.java
@@ -12,8 +12,10 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.ResourceAccessException;
@@ -29,100 +31,128 @@ class RtvEuroAgdScraperWorker implements ScraperWorker {
     private static final String BASE_URL = "https://www.euro.com.pl/rest/api/products/search";
 
     private final RestClient restClient;
+    private final Semaphore rtvEuroAgdSemaphore;
 
+    private static final int PAGE_SIZE = 25;
+
+    @Async
     @Override
     public CompletableFuture<List<Product>> scrapeCategory(Category category, String categoryLocator) {
-        final var PAGE_SIZE = 25;
-        var currentStartFrom = 0;
+        var isSemaphorePermitAcquired = false;
 
-        var products = new ArrayList<Product>();
+        try {
+            rtvEuroAgdSemaphore.acquire();
+            isSemaphorePermitAcquired = true;
 
-        while (true) {
-            try {
-                var uri = UriComponentsBuilder.fromUriString(BASE_URL)
-                        .queryParam("numberOfItems", PAGE_SIZE)
-                        .queryParam("startFrom", currentStartFrom)
-                        .queryParam("category", categoryLocator)
-                        .queryParam("developSearchMode", "false")
-                        .build()
-                        .toUri();
+            var currentStartFrom = 0;
+            var products = new ArrayList<Product>();
 
-                var productResponses = this.restClient
-                        .get()
-                        .uri(uri)
-                        .header("Accept", "application/json")
-                        .header("User-Agent", RandomUserAgentGenerator.getNext())
-                        .retrieve()
-                        .body(RtvEuroAgdResponse.class);
-                if (productResponses == null
-                        || productResponses.results() == null
-                        || productResponses.results().isEmpty()) break;
+            while (true) {
+                var productPage = fetchProductPage(currentStartFrom, category, categoryLocator);
+                if (productPage == null
+                        || productPage.results() == null
+                        || productPage.results().isEmpty()) break;
 
-                productResponses.results().forEach(productResponse -> {
-                    var outletPrices = productResponse
-                            .outletDetails()
-                            .map(outletDetails -> outletDetails.outletCategories().stream()
-                                    .map(RtvEuroAgdProduct.OutletCategory::price)
-                                    .toList());
-                    var price = getLowestPrice(productResponse.prices(), outletPrices);
-
-                    if (productResponse.eanCodes().isEmpty() || price == null) return;
-
-                    var promoCode = productResponse
-                            .voucherDetails()
-                            .map(RtvEuroAgdProduct.VoucherDetails::voucherCode)
-                            .orElse(null);
-                    var condition = productResponse
-                            .outletDetails()
-                            .map(outletDetails -> Condition.OUTLET)
-                            .orElse(Condition.NEW);
-
-                    var priceStamp = new PriceStamp(price, "PLN", condition);
-                    priceStamp.setPromoCode(promoCode);
-
-                    var offer = new Offer(CURRENT_SHOP, getProductUrl(productResponse.identifiers()));
-                    offer.getPriceHistory().add(priceStamp);
-
-                    var ean = productResponse.eanCodes().getFirst();
-                    var images = getBigImages(productResponse.images());
-
-                    var productEntity = new Product(ean, productResponse.name(), category);
-                    productEntity.setImages(images);
-                    productEntity.getOffers().add(offer);
-
-                    products.add(productEntity);
-                });
+                var newProducts = productPage.results().stream()
+                        .map(productResponse -> createProduct(category, productResponse))
+                        .filter(Objects::nonNull)
+                        .toList();
+                products.addAll(newProducts);
 
                 currentStartFrom += PAGE_SIZE;
                 ScrapingUtil.sleep();
-            } catch (HttpStatusCodeException e) {
-                log.warn(
-                        "http error has occurred while scraping category {} on page {}- {}",
-                        category,
-                        currentStartFrom,
-                        e.getStatusCode().value());
-            } catch (ResourceAccessException e) {
-                log.warn(
-                        "timeout occurred in Rtv Euro Agd scraper while scraping category {} on page {} - {}",
-                        category,
-                        currentStartFrom,
-                        e.getMessage());
-            } catch (Exception e) {
-                log.error(
-                        "unexpected error of class {} has occurred while scraping category {} on page {} - {}",
-                        e.getClass(),
-                        category,
-                        currentStartFrom,
-                        e.getMessage());
             }
-        }
 
-        return CompletableFuture.completedFuture(products);
+            return CompletableFuture.completedFuture(products);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("thread was interrupted while acquiring semaphore for scraping category {}", category);
+
+            return CompletableFuture.failedFuture(e);
+        } finally {
+            if (isSemaphorePermitAcquired) rtvEuroAgdSemaphore.release();
+        }
     }
 
     @Override
     public Shop getShop() {
         return CURRENT_SHOP;
+    }
+
+    private RtvEuroAgdResponse fetchProductPage(int currentStartFrom, Category category, String categoryLocator) {
+        var uri = UriComponentsBuilder.fromUriString(BASE_URL)
+                .queryParam("numberOfItems", PAGE_SIZE)
+                .queryParam("startFrom", currentStartFrom)
+                .queryParam("category", categoryLocator)
+                .queryParam("developSearchMode", "false")
+                .build()
+                .toUri();
+
+        try {
+            return restClient
+                    .get()
+                    .uri(uri)
+                    .header("Accept", "application/json")
+                    .header("User-Agent", RandomUserAgentGenerator.getNext())
+                    .retrieve()
+                    .body(RtvEuroAgdResponse.class);
+        } catch (HttpStatusCodeException e) {
+            log.warn(
+                    "http error has occurred while scraping category {} on page {} - {}",
+                    category,
+                    currentStartFrom,
+                    e.getStatusCode().value());
+        } catch (ResourceAccessException e) {
+            log.warn(
+                    "timeout occurred while scraping category {} on page {} - {}",
+                    category,
+                    currentStartFrom,
+                    e.getMessage());
+        } catch (Exception e) {
+            log.error(
+                    "unexpected error of class {} has occurred while scraping category {} on page {} - {}",
+                    e.getClass(),
+                    category,
+                    currentStartFrom,
+                    e.getMessage());
+        }
+
+        return null;
+    }
+
+    private Product createProduct(Category category, RtvEuroAgdProduct productResponse) {
+        var outletPrices = productResponse
+                .outletDetails()
+                .map(outletDetails -> outletDetails.outletCategories().stream()
+                        .map(RtvEuroAgdProduct.OutletCategory::price)
+                        .toList());
+        var price = getLowestPrice(productResponse.prices(), outletPrices);
+
+        if (productResponse.eanCodes().isEmpty() || price == null) return null;
+
+        var promoCode = productResponse
+                .voucherDetails()
+                .map(RtvEuroAgdProduct.VoucherDetails::voucherCode)
+                .orElse(null);
+        var condition = productResponse
+                .outletDetails()
+                .map(outletDetails -> Condition.OUTLET)
+                .orElse(Condition.NEW);
+
+        var priceStamp = new PriceStamp(price, "PLN", condition);
+        priceStamp.setPromoCode(promoCode);
+
+        var offer = new Offer(CURRENT_SHOP, getProductUrl(productResponse.identifiers()));
+        offer.getPriceHistory().add(priceStamp);
+
+        var ean = productResponse.eanCodes().getFirst();
+        var images = getBigImages(productResponse.images());
+
+        var productEntity = new Product(ean, productResponse.name(), category);
+        productEntity.getImages().addAll(images);
+        productEntity.getOffers().add(offer);
+
+        return productEntity;
     }
 
     private BigDecimal getLowestPrice(RtvEuroAgdProduct.Prices prices, Optional<List<Long>> outletPrices) {
