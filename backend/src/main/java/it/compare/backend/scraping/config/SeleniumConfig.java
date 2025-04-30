588b9de3bdf5ed2550ff0f2ef0678602c77f5cec
Paw565pl <paw565pl@gmail.com>
2025-04-30 16:22:35 +0200

    feat: update selenium config

--- backend/src/main/java/it/compare/backend/scraping/config/SeleniumConfig.java
+++ backend/src/main/java/it/compare/backend/scraping/config/SeleniumConfig.java
@@ -1,6 +1,7 @@
 package it.compare.backend.scraping.config;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
@@ -23,26 +24,29 @@ public class SeleniumConfig {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
-        options.addArguments("--disable-extensions");
         options.addArguments("--disable-images");
         options.addArguments("--blink-settings=imagesEnabled=false");
+        options.addArguments("--disable-audio-output");
+        options.addArguments("--disable-webrtc");
+        options.addArguments("--disable-sync");
+        options.addArguments("--disable-default-apps");
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.addArguments("--log-level=3");
 
         var ublockOriginLiteCrx = new File("/app/ublockOriginLite.crx");
-        options.addExtensions(ublockOriginLiteCrx);
+        if (ublockOriginLiteCrx.exists() && ublockOriginLiteCrx.isFile()) options.addExtensions(ublockOriginLiteCrx);
 
         var userAgent =
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36";
         options.addArguments("--user-agent=" + userAgent);
 
         var prefs = Map.of("profile.managed_default_content_settings.stylesheets", 2);
         options.setExperimentalOption("prefs", prefs);
-        options.setExperimentalOption("excludeSwitches", new String[] {"enable-automation"});
+        options.setExperimentalOption("excludeSwitches", List.of("enable-automation"));
         options.setExperimentalOption("useAutomationExtension", false);
 
         options.addArguments("--lang=pl-PL");
-        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--window-size=1440,900");
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--allow-running-insecure-content");
 
