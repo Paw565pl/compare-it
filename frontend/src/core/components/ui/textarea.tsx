994e26bbdf4528322d5328f8e156c5df60a3b9e6
Paw565pl <paw565pl@gmail.com>
2025-05-01 11:09:36 +0200

    feat: allow bigger size and add prop for initiala word count

--- frontend/src/core/components/ui/textarea.tsx
+++ frontend/src/core/components/ui/textarea.tsx
@@ -3,22 +3,24 @@ import { ComponentProps } from "react";
 import { useDebounce } from "use-debounce";
 
 interface TextareaProps extends ComponentProps<"textarea"> {
+  readonly initialWordCount?: number;
   readonly maxWordCount?: number;
 }
 
 export const Textarea = ({
+  initialWordCount,
   maxWordCount,
   className,
   ...props
 }: TextareaProps) => {
-  const [wordCount, setWordCount] = useDebounce(0, 100);
+  const [wordCount, setWordCount] = useDebounce(initialWordCount ?? 0, 100);
 
   return (
     <div className="space-y-2">
       <textarea
         data-slot="textarea"
         className={cn(
-          "border-input placeholder:text-muted-foreground focus-visible:border-primary focus-visible:ring-primary/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive flex field-sizing-content max-h-48 min-h-24 w-full resize-y border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "border-input placeholder:text-muted-foreground focus-visible:border-primary focus-visible:ring-primary/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive flex field-sizing-content max-h-64 min-h-32 w-full resize-y border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className,
         )}
         {...props}
