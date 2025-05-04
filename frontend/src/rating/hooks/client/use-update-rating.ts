c2a2a0f0b848718ee8f0bb9efac6477a94e6f2f7
Paw565pl <paw565pl@gmail.com>
2025-05-04 14:45:26 +0200

    feat: implement optimistic update for updating rating

--- frontend/src/rating/hooks/client/use-update-rating.ts
+++ frontend/src/rating/hooks/client/use-update-rating.ts
@@ -1,11 +1,13 @@
+import { CommentEntity } from "@/comments/entities/comment-entity";
 import { commentsQueryKey } from "@/comments/hooks/query-options/comments-query-key";
 import { getQueryClient } from "@/core/libs/tanstack-query";
 import { apiService } from "@/core/services/api";
 import { ErrorResponse } from "@/core/services/api/types/error-response";
+import { PaginatedData } from "@/core/services/api/types/paginated-data";
 import { productsQueryKey } from "@/products/hooks/query-options/products-query-key";
 import { RatingDto } from "@/rating/dto/rating-dto";
 import { RatingEntity } from "@/rating/entities/rating-entity";
-import { useMutation } from "@tanstack/react-query";
+import { InfiniteData, useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
 const updateRating = async (
@@ -30,8 +32,20 @@ export const useUpdateRating = (
   accessToken: string,
   productId: string,
   commentId: string,
-) =>
-  useMutation<RatingEntity, AxiosError<ErrorResponse>, RatingDto>({
+) => {
+  const queryClient = getQueryClient();
+  const commentPageQueryKey = [
+    ...productsQueryKey,
+    productId,
+    ...commentsQueryKey,
+  ] as const;
+
+  return useMutation<
+    RatingEntity,
+    AxiosError<ErrorResponse>,
+    RatingDto,
+    InfiniteData<PaginatedData<CommentEntity>>
+  >({
     mutationKey: [
       ...productsQueryKey,
       productId,
@@ -42,14 +56,48 @@ export const useUpdateRating = (
     ] as const,
     mutationFn: (ratingDto) =>
       updateRating(accessToken, productId, commentId, ratingDto),
-    onSettled: () => {
-      const queryClient = getQueryClient();
-      const queryKey = [
-        ...productsQueryKey,
-        productId,
-        ...commentsQueryKey,
-      ] as const;
+    onMutate: async (ratingDto) => {
+      await queryClient.cancelQueries({ queryKey: commentPageQueryKey });
 
-      queryClient.invalidateQueries({ queryKey });
+      const previousCommentInfiniteData =
+        queryClient.getQueryData<InfiniteData<PaginatedData<CommentEntity>>>(
+          commentPageQueryKey,
+        );
+
+      const newCommentPages = previousCommentInfiniteData?.pages.map(
+        ({ page, content }) => ({
+          page,
+          content: content.map((comment) => {
+            if (comment.id !== commentId) return comment;
+
+            const newPositiveRatingsCount = ratingDto.isPositive
+              ? comment.positiveRatingsCount + 1
+              : comment.positiveRatingsCount - 1;
+            const newNegativeRatingsCount = ratingDto.isPositive
+              ? comment.negativeRatingsCount - 1
+              : comment.negativeRatingsCount + 1;
+
+            return {
+              ...comment,
+              positiveRatingsCount: newPositiveRatingsCount,
+              negativeRatingsCount: newNegativeRatingsCount,
+            };
+          }),
+        }),
+      );
+      const newCommentInfiniteData = {
+        pageParams: previousCommentInfiniteData?.pageParams,
+        pages: newCommentPages,
+      };
+      queryClient.setQueryData(commentPageQueryKey, newCommentInfiniteData);
+
+      return previousCommentInfiniteData;
+    },
+    onError: (_, __, previousCommentPage) => {
+      queryClient.setQueryData(commentPageQueryKey, previousCommentPage);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: commentPageQueryKey });
     },
   });
+};
