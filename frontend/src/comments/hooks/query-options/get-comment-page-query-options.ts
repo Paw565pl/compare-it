39ee347b568f1133d0063e19e8b8fc65ddd75c09
Paw565pl <paw565pl@gmail.com>
2025-05-04 14:43:33 +0200

    chore: remove needless argument

--- frontend/src/comments/hooks/query-options/get-comment-page-query-options.ts
+++ frontend/src/comments/hooks/query-options/get-comment-page-query-options.ts
@@ -3,7 +3,6 @@ import { commentsQueryKey } from "@/comments/hooks/query-options/comments-query-
 import { apiService } from "@/core/services/api";
 import { ErrorResponse } from "@/core/services/api/types/error-response";
 import { PaginatedData } from "@/core/services/api/types/paginated-data";
-import { PaginationOptions } from "@/core/services/api/types/pagination-options";
 import { productsQueryKey } from "@/products/hooks/query-options/products-query-key";
 import { infiniteQueryOptions } from "@tanstack/react-query";
 import { AxiosError } from "axios";
@@ -12,13 +11,11 @@ const fetchCommentPage = async (
   productId: string,
   pageParam: unknown,
   accessToken?: string,
-  paginationOptions?: PaginationOptions,
 ) => {
   const { data } = await apiService.get<PaginatedData<CommentEntity>>(
     `/api/v1/products/${productId}/comments`,
     {
       params: {
-        ...paginationOptions,
         page: pageParam,
       },
       headers: {
@@ -32,19 +29,13 @@ const fetchCommentPage = async (
 export const getCommentPageQueryOptions = (
   productId: string,
   accessToken?: string,
-  paginationOptions?: PaginationOptions,
 ) =>
   infiniteQueryOptions<PaginatedData<CommentEntity>, AxiosError<ErrorResponse>>(
     {
       // eslint-disable-next-line @tanstack/query/exhaustive-deps
-      queryKey: [
-        ...productsQueryKey,
-        productId,
-        ...commentsQueryKey,
-        paginationOptions,
-      ] as const,
+      queryKey: [...productsQueryKey, productId, ...commentsQueryKey] as const,
       queryFn: ({ pageParam }) =>
-        fetchCommentPage(productId, pageParam, accessToken, paginationOptions),
+        fetchCommentPage(productId, pageParam, accessToken),
       getNextPageParam: ({ page: { number, totalPages } }) =>
         number + 1 < totalPages ? number + 1 : undefined,
       initialPageParam: 0,
--- frontend/src/comments/hooks/server/prefetch-comment-page.ts
+++ frontend/src/comments/hooks/server/prefetch-comment-page.ts
@@ -1,13 +1,11 @@
 import { getCommentPageQueryOptions } from "@/comments/hooks/query-options/get-comment-page-query-options";
-import { PaginationOptions } from "@/core/services/api/types/pagination-options";
 import { QueryClient } from "@tanstack/react-query";
 
 export const prefetchCommentPage = (
   queryClient: QueryClient,
   productId: string,
   accessToken?: string,
-  paginationOptions?: PaginationOptions,
 ) =>
   queryClient.prefetchInfiniteQuery(
-    getCommentPageQueryOptions(productId, accessToken, paginationOptions),
+    getCommentPageQueryOptions(productId, accessToken),
   );
