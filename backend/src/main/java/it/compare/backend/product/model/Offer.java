b2c1fff2274b9794fb19000d1e415d6ac3dbfc49
Paw565pl <paw565pl@gmail.com>
2025-09-18 16:16:06 +0200

    chore: remove url index

--- backend/src/main/java/it/compare/backend/product/model/Offer.java
+++ backend/src/main/java/it/compare/backend/product/model/Offer.java
@@ -3,24 +3,26 @@ package it.compare.backend.product.model;
 import com.mongodb.lang.NonNull;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class Offer {
-    @Indexed
+
+    @NonNull @Indexed
     @Field("shop")
-    @NonNull private Shop shop;
+    private Shop shop;
 
-    @Indexed(unique = true)
-    @Field("url")
-    @NonNull private String url;
+    @NonNull @Field("url")
+    private String url;
 
-    @Field("priceHistory")
-    @NonNull private List<PriceStamp> priceHistory = new ArrayList<>();
+    @NonNull @Field("priceHistory")
+    private List<PriceStamp> priceHistory = new ArrayList<>();
 }
