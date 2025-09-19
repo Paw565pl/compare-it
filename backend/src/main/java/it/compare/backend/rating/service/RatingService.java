6e5a93fc155d2212e21ea2dae0f4801477caf8c6
Paw565pl <paw565pl@gmail.com>
2025-09-19 16:36:10 +0200

    refactor: adjust rating service

--- backend/src/main/java/it/compare/backend/rating/service/RatingService.java
+++ backend/src/main/java/it/compare/backend/rating/service/RatingService.java
@@ -4,11 +4,11 @@ import it.compare.backend.auth.details.OAuthUserDetails;
 import it.compare.backend.auth.repository.UserRepository;
 import it.compare.backend.comment.service.CommentService;
 import it.compare.backend.product.service.ProductService;
-import it.compare.backend.rating.dto.RatingDto;
+import it.compare.backend.rating.dto.RatingRequestDto;
+import it.compare.backend.rating.dto.RatingResponseDto;
 import it.compare.backend.rating.mapper.RatingMapper;
 import it.compare.backend.rating.model.Rating;
 import it.compare.backend.rating.repository.RatingRepository;
-import it.compare.backend.rating.response.RatingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -32,7 +32,7 @@ public class RatingService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Rating not found."));
     }
 
-    public RatingResponse findByAuthorIdAndCommentId(
+    public RatingResponseDto findByAuthorIdAndCommentId(
             OAuthUserDetails oAuthUserDetails, String productId, String commentId) {
         var userId = oAuthUserDetails.getId();
         productService.findProductOrThrow(productId);
@@ -40,12 +40,12 @@ public class RatingService {
 
         var rating =
                 ratingRepository.findByAuthorIdAndCommentId(userId, commentId).orElse(new Rating());
-        return ratingMapper.toResponse(rating);
+        return ratingMapper.toResponseDto(rating);
     }
 
     @Transactional
-    public RatingResponse create(
-            OAuthUserDetails oAuthUserDetails, String productId, String commentId, RatingDto ratingDto) {
+    public RatingResponseDto create(
+            OAuthUserDetails oAuthUserDetails, String productId, String commentId, RatingRequestDto ratingRequestDto) {
         var user = userRepository
                 .findById(oAuthUserDetails.getId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
@@ -53,29 +53,29 @@ public class RatingService {
         var comment = commentService.findCommentOrThrow(commentId);
 
         try {
-            var rating = ratingMapper.toEntity(ratingDto);
+            var rating = ratingMapper.toEntity(ratingRequestDto);
             rating.setAuthor(user);
             rating.setComment(comment);
 
             var savedRating = ratingRepository.save(rating);
-            return ratingMapper.toResponse(savedRating);
+            return ratingMapper.toResponseDto(savedRating);
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityViolationException("You have already rated this comment.");
         }
     }
 
     @Transactional
-    public RatingResponse update(
-            OAuthUserDetails oAuthUserDetails, String productId, String commentId, RatingDto ratingDto) {
+    public RatingResponseDto update(
+            OAuthUserDetails oAuthUserDetails, String productId, String commentId, RatingRequestDto ratingRequestDto) {
         var userId = oAuthUserDetails.getId();
         productService.findProductOrThrow(productId);
         commentService.findCommentOrThrow(commentId);
 
         var rating = findRatingOrThrow(userId, commentId);
-        rating.setIsPositive(ratingDto.isPositive());
+        rating.setIsPositive(ratingRequestDto.isPositive());
 
         var savedRating = ratingRepository.save(rating);
-        return ratingMapper.toResponse(savedRating);
+        return ratingMapper.toResponseDto(savedRating);
     }
 
     @Transactional
