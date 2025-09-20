f757eb419198f04d3647246d99e540a48458fd1f
Paw565pl <paw565pl@gmail.com>
2025-09-20 11:00:15 +0200

    refactor: simplify calculating computed state

--- backend/src/main/java/it/compare/backend/product/model/ComputedState.java
+++ backend/src/main/java/it/compare/backend/product/model/ComputedState.java
@@ -5,6 +5,7 @@ import com.mongodb.lang.Nullable;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Comparator;
+import java.util.Optional;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,29 +30,32 @@ public class ComputedState {
 
     public static ComputedState fromProduct(Product product) {
         var computedState = new ComputedState();
-
         var cutOff = Instant.now().minus(AVAILABILITY_DAYS_THRESHOLD);
-        var availableOffers = product.getOffers().stream()
-                .filter(o -> !o.getPriceHistory().isEmpty()
-                        && !o.getPriceHistory().getLast().getTimestamp().isBefore(cutOff))
-                .toList();
-        var availableOffersSize = availableOffers.size();
 
-        var lowestOffer = availableOffers.stream()
-                .min(Comparator.comparing(o -> o.getPriceHistory().getLast().getPrice()))
+        var latestAvailableOffers = product.getOffers().stream()
+                .map(offer -> offer.getPriceHistory().stream()
+                        .max(Comparator.comparing(PriceStamp::getTimestamp))
+                        .map(latestPrice -> new OfferWithLatestPrice(offer, latestPrice)))
+                .flatMap(Optional::stream)
+                .filter(o -> o.latestPriceStamp().getTimestamp().isAfter(cutOff))
+                .toList();
+        var availableOffersSize = latestAvailableOffers.size();
+
+        var lowestOffer = latestAvailableOffers.stream()
+                .min(Comparator.comparing(o -> o.latestPriceStamp().getPrice()))
                 .orElse(null);
         if (lowestOffer == null) return computedState;
 
-        var lowestOfferPriceStamp = lowestOffer.getPriceHistory().getLast();
-
         computedState.setAvailableOffersCount(availableOffersSize);
         computedState.setBestOffer(new BestOffer(
-                lowestOffer.getShop(),
-                lowestOffer.getUrl(),
-                lowestOfferPriceStamp.getPrice(),
-                lowestOfferPriceStamp.getCurrency(),
-                lowestOfferPriceStamp.getCondition()));
+                lowestOffer.offer().getShop(),
+                lowestOffer.offer().getUrl(),
+                lowestOffer.latestPriceStamp().getPrice(),
+                lowestOffer.latestPriceStamp().getCurrency(),
+                lowestOffer.latestPriceStamp().getCondition()));
 
         return computedState;
     }
+
+    private record OfferWithLatestPrice(Offer offer, PriceStamp latestPriceStamp) {}
 }
