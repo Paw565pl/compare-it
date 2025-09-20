d9e9cf82f631644884df6e7b9e424470fc5abbe7
Paw565pl <paw565pl@gmail.com>
2025-09-20 11:00:57 +0200

    fix: use latest pricestamp for offer's isAvailable field

--- backend/src/main/java/it/compare/backend/product/dto/ProductDetailResponseDto.java
+++ backend/src/main/java/it/compare/backend/product/dto/ProductDetailResponseDto.java
@@ -3,6 +3,7 @@ package it.compare.backend.product.dto;
 import it.compare.backend.product.model.*;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -20,10 +21,12 @@ record OfferResponseDto(@NonNull Shop shop, @NonNull String url, @NonNull List<P
     public boolean getIsAvailable() {
         if (priceHistory.isEmpty()) return false;
 
-        return priceHistory
-                .getLast()
-                .timestamp()
-                .isAfter(Instant.now().minus(ComputedState.AVAILABILITY_DAYS_THRESHOLD));
+        return priceHistory.stream()
+                .max(Comparator.comparing(PriceStampResponseDto::timestamp))
+                .map(priceStampResponseDto -> priceStampResponseDto
+                        .timestamp()
+                        .isAfter(Instant.now().minus(ComputedState.AVAILABILITY_DAYS_THRESHOLD)))
+                .orElse(false);
     }
 }
 
