87343cded4634e217a4871eb1ccfe28cdf1592e2
Paw565pl <paw565pl@gmail.com>
2025-09-23 14:38:35 +0200

    feat: use enum types in query options

--- frontend/src/products/dtos/product-filters-dto.ts
+++ frontend/src/products/dtos/product-filters-dto.ts
@@ -1,7 +1,11 @@
+import { CategoryEntity } from "@/products/entities/category-entity";
+import { ShopEntity } from "@/products/entities/shop-entity";
+
 export interface ProductFiltersDto {
   name: string | null;
-  category: string | null;
-  shop: string | null;
+  category: CategoryEntity | null;
+  shops: ShopEntity[] | null;
   minPrice: number | null;
   maxPrice: number | null;
+  isAvailable: boolean | null;
 }
