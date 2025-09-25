c11b21315881086af91dbd44380dae677b7e694a
Paw565pl <paw565pl@gmail.com>
2025-09-25 15:19:38 +0200

    feat: return skeleton if session is loading

--- frontend/src/core/components/navbar/auth-section.tsx
+++ frontend/src/core/components/navbar/auth-section.tsx
@@ -3,12 +3,19 @@
 import { ProfileLink } from "@/core/components/navbar/profile-link";
 import { SignInButton } from "@/core/components/navbar/sign-in-button";
 import { SignOutButton } from "@/core/components/navbar/sign-out-button";
+import { Skeleton } from "@/core/components/ui/skeleton";
 import { useSession } from "next-auth/react";
 
 export const AuthSection = () => {
   const { data: session, status } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading")
+    return (
+      <div className="border-primary border-b-2 px-2 py-4 sm:p-4">
+        <Skeleton className="h-9 w-31" />
+      </div>
+    );
+
   if (!session) return <SignInButton />;
 
   return (
