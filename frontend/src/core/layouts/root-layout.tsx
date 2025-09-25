5733a4f0802c621b1302eb0f60940b0b8fca3f41
Paw565pl <paw565pl@gmail.com>
2025-09-25 15:04:00 +0200

    style: use single padding

--- frontend/src/core/layouts/root-layout.tsx
+++ frontend/src/core/layouts/root-layout.tsx
@@ -26,7 +26,7 @@ export const RootLayout = ({ children }: RootLayoutProps) => {
       <body className="antialiased">
         <Providers>
           <Navbar />
-          <main className="bg-background container mx-auto px-2 pt-6 pb-4 sm:pt-8">
+          <main className="bg-background container mx-auto px-2 py-4">
             {children}
           </main>
           <Toaster closeButton />
