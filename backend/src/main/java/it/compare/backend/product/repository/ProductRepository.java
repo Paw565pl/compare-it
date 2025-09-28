7be7017b1a70b41f5f0615264b2f2c05a9455db1
Paw565pl <paw565pl@gmail.com>
2025-09-28 13:48:50 +0200

    fix: unstable pagination

--- backend/src/main/java/it/compare/backend/product/repository/ProductRepository.java
+++ backend/src/main/java/it/compare/backend/product/repository/ProductRepository.java
@@ -3,12 +3,12 @@ package it.compare.backend.product.repository;
 import it.compare.backend.product.model.Product;
 import java.time.Instant;
 import java.util.List;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
     List<Product> findAllByEanIn(List<String> eanList);
 
-    Page<Product> findAllByUpdatedAtBefore(Instant cutOff, Pageable pageable);
+    Slice<Product> findAllByUpdatedAtBefore(Instant cutOff, Pageable pageable);
 }
--- backend/src/main/java/it/compare/backend/product/scheduler/ProductComputedStateScheduler.java
+++ backend/src/main/java/it/compare/backend/product/scheduler/ProductComputedStateScheduler.java
@@ -6,9 +6,9 @@ import it.compare.backend.product.repository.ProductRepository;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -37,25 +37,17 @@ class ProductComputedStateRefreshComponent {
     @Async
     public void recalculateStateForStaleProducts() {
         log.info("started recalculating state for stale products");
-        var cutOff = Instant.now().minus(ComputedState.AVAILABILITY_DAYS_THRESHOLD);
 
-        Pageable pageable = PageRequest.of(0, BATCH_SIZE);
-        Page<Product> page;
+        var cutOff = Instant.now().minus(ComputedState.AVAILABILITY_DAYS_THRESHOLD);
+        var pageable = PageRequest.of(0, BATCH_SIZE, Sort.by("_id").ascending());
+        Slice<Product> products;
 
         do {
-            page = productRepository.findAllByUpdatedAtBefore(cutOff, pageable);
-            var products = page.getContent().stream()
-                    .map(product -> {
-                        var computedState = ComputedState.fromProduct(product);
-                        product.setComputedState(computedState);
+            products = productRepository.findAllByUpdatedAtBefore(cutOff, pageable);
+            products.forEach(product -> product.setComputedState(ComputedState.fromProduct(product)));
 
-                        return product;
-                    })
-                    .toList();
-
-            if (!products.isEmpty()) productRepository.saveAll(products);
-            pageable = page.nextPageable();
-        } while (page.hasNext());
+            productRepository.saveAll(products);
+        } while (products.hasContent());
 
         log.info("finished recalculating state for stale products");
     }
